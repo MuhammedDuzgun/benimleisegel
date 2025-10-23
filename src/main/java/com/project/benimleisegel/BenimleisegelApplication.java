3d485b39254904426f2d25cfd2c71b4518d5444b
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-10-23 19:42:59 +0300

    Initial commit

---
+++ src/main/java/com/project/benimleisegel/BenimleisegelApplication.java
@@ -0,0 +1,14 @@
+package com.project.benimleisegel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenimleisegelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenimleisegelApplication.class, args);
+
+	}
+
+}
