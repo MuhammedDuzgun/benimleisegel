47926386f6120c4f5befb95b61455bd0c7547d84
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-10-23 23:04:59 +0300

    add custom methods to UserRepository

--- src/main/java/com/project/benimleisegel/repository/UserRepository.java
+++ src/main/java/com/project/benimleisegel/repository/UserRepository.java
@@ -4,4 +4,6 @@ import com.project.benimleisegel.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+    boolean existsByPhone(String phone);
 }
